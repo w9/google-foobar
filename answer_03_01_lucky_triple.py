d8fd320a2ee7b633262d928d8e34ce0054e145c9
Xun Zhu <zhuxun2@gmail.com>
2017-09-08 13:09:24 -1000

    commit

--- answer_03_01_lucky_triple.py
+++ answer_03_01_lucky_triple.py
@@ -4,23 +4,30 @@ from functools import partial
 
 
 def transition(m, a):
-    """ i.e., matrix-vector multiplication """
+    """
+    It's basically a matrix-vector multiplication optimized
+    for sparse matrices.
+    """
     return [sum(starmap(mul, zip(r, a))) for r in m]
 
 
 def get_trans_mat(l):
-    """ *Divisible and preceeding in list* is a partial order.
+    """
+    *Divisible and preceeding in list* is a partial order.
     This function gives the transition matrice of the corresponding DAG,
     with its diagnal removed.
     """
     return [[1 if i < j and l[j] % l[i] == 0 else 0
-             for j in range(len(l))]
-            for i in range(len(l))]
+             for i in range(j)]
+            for j in range(len(l))]
 
 
 def answer(l):
     trans_mat = get_trans_mat(l)
+    # print(trans_mat)
     walk = partial(transition, trans_mat)
     one_step = walk(list(repeat(1, len(l))))
+    # print(one_step)
     two_step = walk(one_step)
+    # print(two_step)
     return sum(two_step)
