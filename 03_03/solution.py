8158369be6e1845ab79307756281a0cf1d92dbb1
Xun Zhu <zhuxun2@gmail.com>
2017-09-09 12:23:49 -1000

    commit

--- 03_03/solution.py
+++ 03_03/solution.py
@@ -1,9 +1,48 @@
+# ........   0 -> ........   0
+# .......1   1 -> .......1   1
+# ......1.   2 -> ......11   3
+# ......11   3 -> ........   0
+
+# .....1..   4 -> .....1..   4
+# .....1.1   5 -> .......1   1
+# .....11.   6 -> .....111   7
+# .....111   7 -> ........   0
+
+# ....1...   8 -> ....1...   8
+# ....1..1   9 -> .......1   1
+# ....1.1.  10 -> ....1.11  11
+# ....1.11  11 -> ........   0
+
+
+def xor_sum(n):
+    """
+    0 ^ 1 ^ ... ^ (n - 1)
+
+    Note that xor_sum(0) == 0.
+    """
+    if n % 4 == 0:
+        return 0
+    elif n % 4 == 1:
+        return (n - 1)
+    elif n % 4 == 2:
+        return 1
+    else:
+        return n
+
+
 def answer(start, length):
+    """
+    The speed-up trick is:
+
+        k ^ (k+1) ^ ... ^ n   =   (0 ^ 1 ^ ... ^ n) ^ (0 ^ 1 ^ ... ^ (k-1))
+
+    where (0 ^ 1 ^ ... ^ n) can be calculated fast.
+    """
     current = start
     acc = 0
     for i in range(length):
-        for j in range(length):
-            if j < length - i:
-                acc ^= current
-            current += 1
+        line_start = start + i * length
+        line_end = line_start + (length - i)
+        acc ^= xor_sum(line_end) ^ xor_sum(line_start)
+
     return acc
